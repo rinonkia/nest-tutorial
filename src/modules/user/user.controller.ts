be5c85793316576448a819f7b0b46d89038efbbf
rinonkia <git@gatapon.me>
2022-03-07 18:42:57 +0900

    user modules/service/controller/specを作成

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Post()
+  create() {
+    return this.userService.create();
+  }
+}
