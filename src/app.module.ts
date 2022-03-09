17e5d48c5a26e049f592c92fa57d56ffc2408984
rinonkia <git@gatapon.me>
2022-03-09 14:36:54 +0900

    graphqlインストール&設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { AuthorModule } from './modules/author/author.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    UserModule,
+    AuthorModule,
+  ],
 })
 export class AppModule {}
